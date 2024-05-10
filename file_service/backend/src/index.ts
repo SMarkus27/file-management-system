b4f57ed83a79f619805e35ce59155ff2bb9e78d3
SMarkus27 <pereira.markus.27@hotmail.com>
2024-05-10 18:49:10 -0300

    chore: backend file-service upload files

---
+++ file_service/backend/src/index.ts
@@ -0,0 +1,41 @@
+import * as http from "http";
+import {Routes} from "./routes/routes";
+import {logger} from "./utils/log";
+const socket = require("socket.io")
+
+const PORT = 3000;
+
+const handler = (request, response) => {
+    const defaultRoute = async (request, response) => response.end("Hello")
+    const routes = new Routes(io);
+    const chosen = routes[request.method.toLocaleLowerCase()] || defaultRoute;
+
+    return chosen.apply(routes, [request, response])
+}
+
+
+const server = http.createServer(handler)
+
+const io = socket(server, {
+    cors: {
+        origin: "*",
+        credentials: false
+    }
+})
+
+io.on("connection", (socket) => logger.info("someone connected" + socket.id))
+const interval = setInterval(() => {
+    io.emit("file-uploaded", 5e6)
+
+}, 250)
+
+
+const startServer = () => {
+    // @ts-ignore
+    const {address, port} = server.address()
+
+    logger.info(`app running at http://${address}:${port}`)
+
+}
+
+server.listen(PORT, startServer)
---
+++ file_service/backend/src/routes/routes.ts
@@ -0,0 +1,45 @@
+import {UploadHandler} from "../uploadHandler";
+import {logger, pipelineAsync} from "../utils/log";
+
+const url = require("url");
+
+
+export class Routes {
+    #io
+    constructor(io) {
+        this.#io = io;
+    }
+    async options(request, response) {
+        response.writeHead(204, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Method": "OPTIONS, POST",
+
+        });
+        response.end()
+    }
+
+    async post(request, response) {
+
+        const {headers} = request;
+        const {query: {socketId}} = url.parse(request.url, true);
+        const redirectTo = headers.origin;
+
+        const uploadHandler = new UploadHandler(this.#io, socketId);
+        const onFinish = (request, redirectTo) => () =>{
+            response.writeHead(303, {
+                Connection: "close",
+                Location: `${redirectTo}?msg=Files upload with success!`
+            })
+
+            response.end()
+        }
+        const busboyInstance = uploadHandler.registerEvents(headers, onFinish(response, redirectTo))
+
+        await pipelineAsync(
+            request,
+            busboyInstance
+        )
+
+        logger.info("Request finished with success!!")
+    }
+}
