3c2efb6589b79ea8f7149dbd4cbcfcd571950086
SMarkus27 <pereira.markus.27@hotmail.com>
2024-05-11 11:11:48 -0300

    chore: mock controller and route

---
+++ mock/src/routes/files/route.ts
@@ -0,0 +1,15 @@
+import {Router} from "express";
+import {FilesController} from "../../controllers/files/controller";
+
+const filesController = new FilesController();
+
+export const filesRouter = Router()
+
+filesRouter.route("/files")
+    .get(filesController.getOneFile)
+    .post(filesController.createFile)
+
+filesRouter.route("/files/:id")
+    .get(filesController.getFiles)
+    .put(filesController.updateFile)
+    .delete(filesController.deleteFile)
